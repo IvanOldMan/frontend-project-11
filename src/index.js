8085045e5143b4402d60f4cb171b5b26b7c97582
Ivan Starikov <oldstarman@yandex.ru>
2024-08-26 10:37:54 +0300

    STEP_2: fixed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import  './styles.scss';
 import  'bootstrap';
+import * as yup from 'yup';
+import { watchedState } from './view.js';
 
-console.log("Hello World!");
+const form = document.forms[0];
+form.addEventListener('submit', (event) => {
+  console.log(watchedState);
+  event.preventDefault();
+  const formData = new FormData(form);
+
+  const schema = yup.string().required().url();
+
+  schema.validate(formData.get('url').trim())
+    .then((url) => {
+      if (!watchedState.rss.feeds.includes(url)) {
+        watchedState.rss.feeds.push(url);
+        watchedState.rss.valid = 'valid';
+      } else {
+        watchedState.rss.valid = 'invalid';
+        watchedState.rss.errors = 'double';
+      }
+
+
+    })
+    .catch((err) => {
+      watchedState.rss.valid = 'invalid';
+      watchedState.rss.errors = err;
+    });
+})
+
