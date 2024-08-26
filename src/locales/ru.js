068bbf65ee7d1f4349bb5898a74306d223afad08
Ivan Starikov <oldstarman@yandex.ru>
2024-08-26 15:04:29 +0300

    STEP_3: added

---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    elements: {
+      title: 'RSS агрегатор',
+      lead: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      placeholder: 'Ссылка RSS',
+      label: 'Ссылка RSS',
+      button: 'Добавить',
+      example: 'Пример: https://lorem-rss.hexlet.app/feed',
+    },
+    errors: {
+      double: 'RSS уже существует',
+      validate: 'Ссылка должна быть валидным URL',
+    }
+  },
+}
