9ccbc9f75e64327e7c86708d759a2cebb032c268
Ivan Starikov <oldstarman@yandex.ru>
2024-09-02 14:32:27 +0300

    STEP_7: fixed

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ const config = {
     mode: isProduction ? 'production' : 'development',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     devServer: {
         open: true,
@@ -35,30 +35,34 @@ const config = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-                exclude: /node_modules/
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
             },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
+            { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
             {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader?limit=10000',
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader',
+            },
         ],
     },
 };
 
 module.exports = () => {
+
     if (isProduction) {
         config.mode = 'production';
         
